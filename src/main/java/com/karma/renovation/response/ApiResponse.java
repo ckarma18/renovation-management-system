7835b3fc52b85a123635bc3f91c4b866c9122fa0
Karma <ckarma18@gmail.com>
2026-07-28 13:37:38 +0545

    Add generic API response wrapper

--- src/main/java/com/karma/renovation/response/ApiResponse.java
+++ src/main/java/com/karma/renovation/response/ApiResponse.java
@@ -1,4 +1,16 @@
 package com.karma.renovation.response;
 
-public class ApiResponse {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T> {
+
+    private boolean success;
+    private String message;
+    private T data;
+
 }
